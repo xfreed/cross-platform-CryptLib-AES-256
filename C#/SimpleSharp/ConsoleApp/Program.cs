095da6cd081c163ad6cb83ed0ec2872fa5e78f56
Michael K <skilledaligator@gmail.com>
2018-10-27 13:33:39 +0300

    Added C# Crypt/Decrypt

---
+++ C#/SimpleSharp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "Simple text";
+            String iv = CryptLib.GenerateRandomIV(16);
+            string key = CryptLib.getHashSha256("Your key", 32);
+            text = CryptLib.Encrypt(text, key, iv);
+            Console.WriteLine(text);
+            Console.WriteLine(CryptLib.Decrypt(text, key));
+        }
+    }
+}
